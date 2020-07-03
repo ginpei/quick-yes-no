e891e68dcf6b02d096865a834e8a81f9673cdec0
ginpei <ginpei@gmail.com>
2020-07-02 20:45:34 -0700

    build(eslint, prettier): install and setup

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: [
+    'eslint-config-airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'off', // leave it to tsc
+    '@typescript-eslint/no-use-before-define': 'off',
+    'arrow-parens': ['error', 'always'],
+    'class-methods-use-this': 'off',
+    'import/extensions': ['error', 'never'],
+    'import/prefer-default-export': 'off',
+  },
+};
