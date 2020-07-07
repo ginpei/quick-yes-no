3dfefff66dd26900282457d460f892809fcab267
ginpei <ginpei@gmail.com>
2020-07-07 01:15:47 -0700

    refactor(decision flicker): move decided item under category

--- components/DecisionCategoryView.tsx
+++ components/DecisionCategoryView.tsx
@@ -1,12 +1,37 @@
-import { CSSProperties, useMemo } from 'react';
+import { CSSProperties, RefObject, useEffect, useMemo, useRef } from 'react';
+import { Candidate } from '../models/Candidate';
+import { Pos } from '../models/Length';
+import { animate } from '../util/animate';
 import { cssVar } from '../util/cssVar';
+import { sleep } from '../util/sleep';
+import { CandidateImage } from './CandidateImage';
 import styles from './DecisionCategoryView.module.scss';
 import { CategoryLayout } from './DecisionFlicker';
 
+const noop = () => {
+  /* none */
+};
+
+// TODO move to config placed somewhere in the future
+const animationDuration = 300;
+const imageWidth = 64;
+
+// TODO replace with state somehow
+let activeAnimationId = 0;
+
+/**
+ * 1. Print category block
+ * 2. Print and animate decided candidate with category block
+ */
 export const DecisionCategoryView: React.FC<{
+  candidateTransition: Pos;
+  decidedCandidate: Candidate | null;
   hovered: boolean;
   layout: CategoryLayout;
-}> = ({ hovered, layout }) => {
+}> = ({ candidateTransition, decidedCandidate, hovered, layout }) => {
+  const refCategory = useRef<HTMLDivElement>(null);
+  const refCandidate = useRef<HTMLDivElement>(null);
+
   const style: CSSProperties = useMemo(
     () =>
       cssVar({
@@ -16,15 +41,172 @@ export const DecisionCategoryView: React.FC<{
       }),
     [layout]
   );
+
+  const candidatePos: Pos = useMemo(
+    () => ({
+      x: candidateTransition.x - layout.x,
+      y: candidateTransition.y - layout.y,
+    }),
+    [layout, candidateTransition]
+  );
+
+  useEffect(() => {
+    const elCategory = refCategory.current;
+    if (!elCategory || !refCandidate.current || !decidedCandidate) {
+      return noop;
+    }
+
+    const animationId = Math.random();
+    activeAnimationId = animationId;
+
+    animateChosenCategory(
+      elCategory,
+      refCandidate.current,
+      layout,
+      candidateTransition,
+      animationId
+    );
+
+    return () => resetChosenCategoryAnimation(elCategory);
+  }, [
+    refCategory.current,
+    refCandidate.current,
+    decidedCandidate,
+    layout,
+    candidateTransition,
+  ]);
+
   return (
     <div className={styles.root} style={style}>
-      <div
-        className={styles.content}
-        data-category-id={layout.category.name}
-        data-hovered={hovered}
-      >
+      <div className={styles.content} data-hovered={hovered} ref={refCategory}>
         {layout.category.name}
+      </div>
+      <LastCandidateView
+        candidate={decidedCandidate}
+        refAnimationTarget={refCandidate}
+        pos={candidatePos}
+      />
+    </div>
+  );
+};
+
+const LastCandidateView: React.FC<{
+  candidate: Candidate | null;
+  refAnimationTarget: RefObject<HTMLDivElement>;
+  pos: Pos;
+}> = ({ candidate, refAnimationTarget, pos }) => {
+  const style: CSSProperties = useMemo(
+    () =>
+      cssVar({
+        '--LastCandidateView-left': `${pos.x}px`,
+        '--LastCandidateView-top': `${pos.y}px`,
+      }),
+    [pos]
+  );
+
+  const candidateImageStyle: CSSProperties = useMemo(
+    () => cssVar({ '--CandidateImage-width': `${imageWidth}px` }),
+    []
+  );
+
+  return (
+    <div className={styles.LastCandidateView} style={style}>
+      <div ref={refAnimationTarget}>
+        {candidate && (
+          <CandidateImage
+            candidate={candidate}
+            className={styles.candidateView}
+            style={candidateImageStyle}
+          />
+        )}
       </div>
     </div>
   );
 };
+
+async function animateChosenCategory(
+  elCategory: HTMLElement,
+  elCandidate: HTMLElement,
+  destination: CategoryLayout,
+  offset: Pos,
+  animationId: number
+) {
+  // how to make them better?
+
+  const wholeDuration = animationDuration;
+
+  elCategory.setAttribute('data-eating', 'true');
+
+  const x = destination.x - offset.x;
+  const y = destination.y - offset.y - imageWidth / 2;
+
+  animate(
+    elCandidate,
+    [
+      { opacity: '1', transform: 'scale(1)' },
+      {
+        opacity: '0',
+        transform: `translate(${x}px, ${y}px) scale(0.5)`,
+      },
+    ],
+    {
+      duration: animationDuration,
+      easing: 'ease-in',
+      fill: 'forwards',
+    }
+  );
+
+  await sleep(animationDuration / 2);
+
+  if (activeAnimationId !== animationId) {
+    return;
+  }
+
+  const anim1 = animate(
+    elCategory,
+    [{ transform: 'scale(1)' }, { transform: 'scale(0.5)' }],
+    {
+      duration: wholeDuration / 3,
+      easing: 'ease-out',
+      fill: 'forwards',
+    }
+  );
+
+  await anim1.finished;
+
+  if (activeAnimationId !== animationId) {
+    animate(
+      elCategory,
+      [{ transform: 'scale(1)' }, { transform: 'scale(1)' }],
+      {
+        fill: 'forwards',
+      }
+    );
+
+    return;
+  }
+
+  const anim2 = animate(
+    elCategory,
+    [{ transform: 'scale(0.5)' }, { transform: 'scale(1)' }],
+    {
+      duration: (wholeDuration * 2) / 3,
+      easing: 'cubic-bezier(.2,3,1,1.2)',
+      fill: 'forwards',
+    }
+  );
+
+  await anim2.finished;
+
+  await sleep(animationDuration);
+
+  if (activeAnimationId !== animationId) {
+    return;
+  }
+
+  elCategory.removeAttribute('data-eating');
+}
+
+function resetChosenCategoryAnimation(elCategory: HTMLElement) {
+  elCategory.removeAttribute('data-eating');
+}
--- components/DecisionFlicker.tsx
+++ components/DecisionFlicker.tsx
@@ -1,10 +1,8 @@
-import { CSSProperties, useCallback, useMemo, useRef, useState } from 'react';
+import { CSSProperties, useCallback, useMemo, useState } from 'react';
 import { Candidate } from '../models/Candidate';
 import { Category } from '../models/Category';
 import { measureDistance, Pos, subtractPos } from '../models/Length';
-import { animate } from '../util/animate';
 import { cssVar } from '../util/cssVar';
-import { sleep } from '../util/sleep';
 import { CandidateImage } from './CandidateImage';
 import { DecisionCategoryView } from './DecisionCategoryView';
 import styles from './DecisionFlicker.module.scss';
@@ -25,10 +23,6 @@ export interface CategoryLayout {
 // TODO move to config placed somewhere in the future
 const animationDuration = 300;
 const imageWidth = 64;
-
-// TODO replace with state somehow
-let activeAnimationId = 0;
-let animatingCategoryName = '';
 
 export const DecisionFlicker: React.FC<{
   candidate: Candidate | null;
@@ -73,29 +67,6 @@ export const DecisionFlicker: React.FC<{
       categoryLayouts.find((v) => v.category.name === nearestCategory.name) ??
       null;
 
-    const elCategory = document.querySelector(
-      `[data-category-id="${nearestCategory.name}"]`
-    );
-    const elCandidate = document.querySelector('[data-js-decided-candidate]');
-
-    if (
-      elCategory instanceof HTMLElement &&
-      elCandidate instanceof HTMLElement &&
-      categoryLayout
-    ) {
-      const animationId = Date.now();
-      activeAnimationId = animationId;
-      animatingCategoryName = categoryLayout.category.name;
-
-      animateChosenCategory(
-        elCategory,
-        elCandidate,
-        categoryLayout,
-        candidateTransition,
-        animationId
-      );
-    }
-
     setLastCandidateTransition(candidateTransition);
     setLastCandidate(candidate);
     setLastCategoryLayout(categoryLayout);
@@ -114,6 +85,12 @@ export const DecisionFlicker: React.FC<{
     >
       {categoryLayouts.map((layout) => (
         <DecisionCategoryView
+          candidateTransition={lastCandidateTransition}
+          decidedCandidate={
+            layout.category.name === lastCategoryLayout?.category.name
+              ? lastCandidate
+              : null
+          }
           hovered={layout.category === nearestCategory}
           key={layout.category.name}
           layout={layout}
@@ -127,10 +104,6 @@ export const DecisionFlicker: React.FC<{
           onTransition={onCandidateTransition}
         />
       )}
-      <LastCandidateView
-        candidate={lastCandidate}
-        transition={lastCandidateTransition}
-      />
     </div>
   );
 };
@@ -182,132 +155,6 @@ const CurrentCandidateView: React.FC<{
     </div>
   );
 };
-
-const LastCandidateView: React.FC<{
-  candidate: Candidate | null;
-  transition: Pos;
-}> = ({ candidate, transition }) => {
-  const ref = useRef<HTMLDivElement>(null);
-
-  const style: CSSProperties = useMemo(
-    () =>
-      cssVar({
-        '--CurrentCandidateView-left': `${transition.x}px`,
-        '--CurrentCandidateView-top': `${transition.y}px`,
-      }),
-    [transition]
-  );
-
-  const candidateImageStyle: CSSProperties = useMemo(
-    () => cssVar({ '--CandidateImage-width': `${imageWidth}px` }),
-    []
-  );
-
-  return (
-    <div className={styles.LastCandidateView} style={style}>
-      <div data-js-decided-candidate>
-        {candidate && (
-          <CandidateImage
-            candidate={candidate}
-            className={styles.candidateView}
-            style={candidateImageStyle}
-          />
-        )}
-      </div>
-    </div>
-  );
-};
-
-async function animateChosenCategory(
-  elCategory: HTMLElement,
-  elCandidate: HTMLElement,
-  destination: CategoryLayout,
-  offset: Pos,
-  animationId: number
-) {
-  // how to make them better?
-
-  const wholeDuration = animationDuration;
-
-  elCategory.setAttribute('data-eating', 'true');
-
-  const x = destination.x - offset.x;
-  const y = destination.y - offset.y - imageWidth / 2;
-
-  const anim0 = animate(
-    elCandidate,
-    [
-      { opacity: '1', transform: 'scale(1)' },
-      {
-        opacity: '0',
-        transform: `translate(${x}px, ${y}px) scale(0.5)`,
-      },
-    ],
-    {
-      duration: animationDuration,
-      easing: 'ease-in',
-      fill: 'forwards',
-    }
-  );
-
-  await sleep(animationDuration / 2);
-
-  if (activeAnimationId !== animationId) {
-    if (animatingCategoryName !== destination.category.name) {
-      elCategory.removeAttribute('data-eating');
-    }
-    return;
-  }
-
-  const anim1 = animate(
-    elCategory,
-    [{ transform: 'scale(1)' }, { transform: 'scale(0.5)' }],
-    {
-      duration: wholeDuration / 3,
-      easing: 'ease-out',
-      fill: 'forwards',
-    }
-  );
-
-  await anim1.finished;
-
-  if (activeAnimationId !== animationId) {
-    if (animatingCategoryName !== destination.category.name) {
-      elCategory.removeAttribute('data-eating');
-      animate(
-        elCategory,
-        [{ transform: 'scale(1)' }, { transform: 'scale(1)' }],
-        {
-          fill: 'forwards',
-        }
-      );
-    }
-    return;
-  }
-
-  const anim2 = animate(
-    elCategory,
-    [{ transform: 'scale(0.5)' }, { transform: 'scale(1)' }],
-    {
-      duration: (wholeDuration * 2) / 3,
-      easing: 'cubic-bezier(.2,3,1,1.2)',
-      fill: 'forwards',
-    }
-  );
-
-  await anim2.finished;
-
-  await sleep(animationDuration);
-
-  if (activeAnimationId !== animationId) {
-    if (animatingCategoryName !== destination.category.name) {
-      elCategory.removeAttribute('data-eating');
-    }
-    return;
-  }
-
-  elCategory.removeAttribute('data-eating');
-}
 
 function createCategoryLayouts(
   categories: Category[],
