2545b76daca1c08ade143ff7cf78c4c42d561593
ginpei <ginpei@gmail.com>
2020-07-11 00:50:52 -0700

    feat(questions): add edit page

---
+++ pages/questions/[questionId]/edit.tsx
@@ -0,0 +1,128 @@
+import firebase from 'firebase/app';
+import { useRouter } from 'next/dist/client/router';
+import Link from 'next/link';
+import { useCallback, useState } from 'react';
+import { BasicLayout } from '../../../src/components/BasicLayout';
+import {
+  QuestionCallback,
+  QuestionForm,
+} from '../../../src/components/QuestionForm';
+import { useFirebaseAuth } from '../../../src/hooks/useFirebaseAuth';
+import { initializeFirebase } from '../../../src/models/firebase';
+import {
+  getQuestionPath,
+  Question,
+  saveQuestion,
+  useLatestQuestion,
+} from '../../../src/models/Question';
+import LoadingPage from '../../../src/screens/LoadingPage';
+import NotFoundPage from '../../../src/screens/NotFoundPage';
+import { sleep } from '../../../src/util/sleep';
+import ErrorPage from '../../../src/screens/ErrorPage';
+
+initializeFirebase();
+const fs = firebase.firestore();
+const auth = firebase.auth();
+
+const QuestionEditPage: React.FC = () => {
+  const router = useRouter();
+  const { questionId } = router.query;
+
+  const [formDisabled, setFormDisabled] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [user, userReady] = useFirebaseAuth(auth);
+
+  if (questionId instanceof Array) {
+    throw new Error('Invalid parameter "questionId"');
+  }
+
+  const [initialQuestion, questionReady, questionError] = useLatestQuestion(
+    fs,
+    (userReady && questionId) || ''
+  );
+
+  const onQuestionSubmit: QuestionCallback = useCallback(
+    async (newQuestion: Question) => {
+      setFormDisabled(true);
+      setErrorMessage('');
+
+      try {
+        const userId = user?.uid;
+        if (!userId) {
+          throw new Error('You must log in');
+        }
+
+        const [savedQuestion] = await Promise.all([
+          saveQuestion(fs, { ...newQuestion, userId }),
+          sleep(500),
+        ]);
+
+        const url = getQuestionPath(savedQuestion);
+        window.location.replace(url.as);
+      } catch (error) {
+        console.log('Question', newQuestion);
+        console.error(error);
+        setErrorMessage(error?.message ?? 'Unknown error');
+        setFormDisabled(false);
+      }
+    },
+    [user]
+  );
+
+  if (typeof questionId !== 'string' || !questionReady || !userReady) {
+    return <LoadingPage />;
+  }
+
+  if (questionError) {
+    return <ErrorPage error={questionError} />;
+  }
+
+  if (!initialQuestion) {
+    return <NotFoundPage />;
+  }
+
+  return (
+    <BasicLayout className="ui-container QuestionEditPage">
+      <h1>Edit</h1>
+      <Link {...getQuestionPath(null)}>
+        <a>Index</a>
+      </Link>
+      {' | '}
+      <Link {...getQuestionPath(initialQuestion, 'view')}>
+        <a>View</a>
+      </Link>
+      {errorMessage && <p className="errorMessage">{errorMessage}</p>}
+      <FormContainer
+        disabled={formDisabled}
+        onSubmit={onQuestionSubmit}
+        question={initialQuestion}
+      />
+    </BasicLayout>
+  );
+};
+
+const FormContainer: React.FC<{
+  disabled: boolean;
+  onSubmit: QuestionCallback;
+  question: Question;
+}> = ({ disabled, onSubmit, question: originalQuestion }) => {
+  const [question, setQuestion] = useState(originalQuestion);
+
+  const onQuestionChange: QuestionCallback = useCallback(
+    (newQuestion: Question) => {
+      setQuestion(newQuestion);
+    },
+    []
+  );
+
+  return (
+    <QuestionForm
+      disabled={disabled}
+      onChange={onQuestionChange}
+      onSubmit={onSubmit}
+      question={question}
+    />
+  );
+};
+
+export default QuestionEditPage;
