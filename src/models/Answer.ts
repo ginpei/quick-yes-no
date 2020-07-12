91bba95c720c0ada3abd4201418a9c45fc7c0a8e
ginpei <ginpei@gmail.com>
2020-07-11 19:36:59 -0700

    fix(question details page): loading did not finish if not logged in

--- src/models/Answer.ts
+++ src/models/Answer.ts
@@ -65,7 +65,7 @@ export function useAnswersOf(
       setAnswer(v);
       setReady(true);
     });
-  }, [fs]);
+  }, [fs, questionId]);
 
   return [answers, ready];
 }
--- src/models/useQuestionPagePrep.tsx
+++ src/models/useQuestionPagePrep.tsx
@@ -1,39 +1,30 @@
 import firebase from 'firebase/app';
 import { ParsedUrlQuery } from 'querystring';
-import { useFirebaseAuth } from '../hooks/useFirebaseAuth';
 import ErrorPage from '../screens/ErrorPage';
 import LoadingPage from '../screens/LoadingPage';
-import NeedLoginPage from '../screens/NeedLoginPage';
 import NotFoundPage from '../screens/NotFoundPage';
 import { initializeFirebase } from './firebase';
 import { Question, useLatestQuestion } from './Question';
 
-export type QuestionPagePrep = [JSX.Element] | [null, Question, firebase.User];
+export type QuestionPagePrep = [JSX.Element] | [null, Question];
 
 initializeFirebase();
-const auth = firebase.auth();
 const fs = firebase.firestore();
 
 export function useQuestionPagePrep(
   id: ParsedUrlQuery[number]
 ): QuestionPagePrep {
-  const [user, userReady] = useFirebaseAuth(auth);
-
   if (id instanceof Array) {
     throw new Error('Invalid parameter "questionId"');
   }
 
   const [question, questionReady, questionError] = useLatestQuestion(
     fs,
-    (user && id) || ''
+    id || ''
   );
 
-  if (typeof id !== 'string' || !questionReady || !userReady) {
+  if (typeof id !== 'string' || !questionReady) {
     return [<LoadingPage />];
-  }
-
-  if (!user) {
-    return [<NeedLoginPage />];
   }
 
   if (questionError) {
@@ -44,5 +35,5 @@ export function useQuestionPagePrep(
     return [<NotFoundPage />];
   }
 
-  return [null, question, user];
+  return [null, question];
 }
